fc00c753c1bcc24e54fdcfafff62b6eaeedd2eb7
Mizu <70519093+Mizumo-prjkt@users.noreply.github.com>
2026-08-04 13:05:01 +0000

    i am still figuring out shit

--- presetup.sh
+++ presetup.sh
@@ -7,8 +7,12 @@ fi
 
 
 function package_man() {
+    echo "deb http://security.ubuntu.com/ubuntu focal-security main universe" > /etc/apt/sources.list.d/ubuntu-focal-sources.list # for libncurses5
     apt update
     apt upgrade -y
+    apt install software-properties-common
+    add-apt-repository ppa:deadsnakes/ppa # we add deadsnakes to get us python2
+    apt update # again
     apt install aria2 libncurses5 git python-is-python3 python2 python3 wget curl libc6-dev tar -y
     apt install cpio default-jdk git-core gnupg flex bison gperf build-essential zip curl aria2 libc6-dev libncurses5-dev x11proto-core-dev libx11-dev libreadline6-dev libgl1-mesa-glx libgl1-mesa-dev python3 make sudo gcc g++ bc grep tofrodos python3-markdown libxml2-utils xsltproc zlib1g-dev -y
     echo "Done installing dependencies"
